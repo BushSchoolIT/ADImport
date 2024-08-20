2b703eb565b3ba97b6d854c00ca5590a3d1fa31e
Michael Lindner <Michael.Lindner@bush.edu>
2024-08-20 16:52:09 -0700

    changed to Write-Output for piping to a log file

--- ADImport.ps1
+++ ADImport.ps1
@@ -11,7 +11,8 @@ try
         GivenName             = $User.'Firstname'
         Surname               = $User.'Lastname'
         DisplayName           = $User.'Firstname' + " " + $User.'Lastname' + " " + $User.'Suffix'
-        UserPrincipalName     = $User.'Firstname' + "." + $User.'Lastname' + "@bush.edu"   
+        UserPrincipalName     = $User.'Firstname' + "." + $User.'Lastname' + "@bush.edu"
+        # SamAccountName is limited to 20 characters. If the name is longer than 20 characters, you will need to manually create the account
         SamAccountName        = $User.'Firstname' + "." + $User.'Lastname' 
         Path                  = "OU=" + $User.'OU' + ",OU=Students,OU=ManUsers,DC=bush,DC=edu"
         Description           = $User.'OU'
@@ -30,19 +31,23 @@ try
     if (Get-ADUser -Filter "UserPrincipalName -eq '$($User.'Firstname' + "." + $User.'Lastname' + "@bush.edu")'") {
 
         # Give a warning if user exists
-        Write-Host "A user with username $($User.'User logon name') already exists in Active Directory." -ForegroundColor Yellow
+        Write-Output "A user with username $($User.'User logon name') already exists in Active Directory."
+	    Write-Output " "
     }
     else {
         # User does not exist then proceed to create the new user account
         # Account will be created in the OU provided by the $User.OU variable read from the CSV file
         New-ADUser @NewUserParams
+        # Set the country code and proxy address for the new user
         Set-ADUser -Identity $($User.'Firstname' + "." + $User.'Lastname') -Replace @{c="US";co="United States";countrycode=840}
         Set-ADUser -Identity $($User.'Firstname' + "." + $User.'Lastname') -add @{ProxyAddresses="SMTP:" + $User.'Firstname' + "." + $User.'Lastname' + "@bush.edu"}
-        Write-Host "The user $($User.'Firstname' + "." + $User.'Lastname' + "@bush.edu") is created successfully." -ForegroundColor Green
+        Write-Output "The user $($User.'Firstname' + "." + $User.'Lastname' + "@bush.edu") is created successfully."
+	    Write-Output " "
     }
     }
     catch {
     # Handle any errors that occur during account creation
-    Write-Host "Failed to create user $($User.'Firstname' + "." + $User.'Lastname' + "@bush.edu") - $($_.Exception.Message)" -ForegroundColor Red
+    Write-Output "Failed to create user $($User.'Firstname' + "." + $User.'Lastname' + "@bush.edu") - $($_.Exception.Message)"
+    Write-Output " "
 }
 }
